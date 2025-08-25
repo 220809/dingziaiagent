82065fd618203e7e83e775989fa4b201dd993f3c
dingzk <110876210+220809@users.noreply.github.com>
2025-08-25 20:14:42 +0800

    初始化项目 引入 hutool 工具类库；引入 knife4j 接口文档工具依赖

---
+++ src/main/java/com/dingzk/dingziaiagent/DingziAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.dingzk.dingziaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DingziAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DingziAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dingzk/dingziaiagent/DingziAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dingzk.dingziaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DingziAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
